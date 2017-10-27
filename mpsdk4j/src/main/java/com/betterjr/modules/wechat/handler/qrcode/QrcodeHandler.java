6f45b17209738de323c10cc1b07e2ae65a648464
Xuyp <xuyp@bytter.com>
2017-10-27 15:41:01 +0800

    init

---
+++ mpsdk4j/src/main/java/com/betterjr/modules/wechat/handler/qrcode/QrcodeHandler.java
@@ -0,0 +1,19 @@
+// Copyright (c) 2014-2016 Bytter. All rights reserved.
+// ============================================================================
+// CURRENT VERSION
+// ============================================================================
+// CHANGE LOG
+// V2.0 : 2016年9月18日, liuwl, creation
+// ============================================================================
+package com.betterjr.modules.wechat.handler.qrcode;
+
+import com.betterjr.modules.wechat.data.event.BasicEvent;
+import com.betterjr.modules.wechat.data.message.BasicMsg;
+
+/**
+ * @author liuwl
+ *
+ */
+public interface QrcodeHandler {
+    public BasicMsg process(BasicEvent anEvent);
+}
